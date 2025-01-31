0242c940dc720ac833c7f237861b74ddc6ef4c12
lith453 <99thompsonp@gmail.com>
2025-01-31 12:19:25 -0600

    initial commit

---
+++ myFirstProgram.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+    int myInt       =32;
+    float myFloat   =3.0928;
+    double myDouble =2.78912;
+    char myChar     ='A';
+    bool myBool     = true;
+    string myString = "Hello, world!";
+    
+    cout << "Integer: " << myInt <<endl;
+    cout << "Float: " << myFloat <<endl;
+    cout << "Double: " << myDouble <<endl;
+    cout << "Character: " << myChar <<endl;
+    cout << "Boolean: " << myBool <<endl;
+    cout << "String: " << myString <<endl;
+
+    return 0;
+}
